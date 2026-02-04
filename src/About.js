d9836eca8a0ffd14ea3aafb4372fbb8fd3d5a595
Jenish <jenish112005@gmail.com>
2026-02-04 05:27:10 +0000

    Fix typo: AI & Machine Learningt to AI & Machine Learning

--- src/About.js
+++ src/About.js
@@ -45,7 +45,7 @@ function About() {
           <div className="row">
             <div className="col-1 text-danger pt-1"><i className="ti-paint-bucket icon-lg"></i></div>
             <div className="col-10 ml-auto mr-3">
-              <h6>AI & Machine Learningt</h6>
+              <h6>AI & Machine Learning</h6>
               <p className="subtitle">Developing intelligent systems, applying advanced models, and extracting actionable insights from data.</p>
               <hr />
             </div>
